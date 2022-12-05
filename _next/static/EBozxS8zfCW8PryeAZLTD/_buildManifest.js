c4e140fa1c7519e14f11f8b5bb9a5a76e292ebca
xnode243 <xnode243@gmail.com>
2022-12-05 11:55:19 +0700

    Deploy gh-pages

---
+++ _next/static/EBozxS8zfCW8PryeAZLTD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
